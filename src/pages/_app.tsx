519662a38c104b80f311191113308f2649a4f9e5
nnthanh01061999 <nnthanh01061999@gmail.com>
2023-03-11 11:53:41 +0700

    add vercel analys

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ModalContextProvider } from "react-modal-handle";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ModalContextProvider>
       <Component {...pageProps} />
+      <Analytics />
     </ModalContextProvider>
   );
 }
